5f66161a332aeed68a986d8a10d137693a4cd7cb
Erjon Thaqi <erth207@gmail.com>
2025-09-01 21:15:09 +0200

    Objects added

---
+++ src/main/java/ch/bbw/et/vehicle/Auto.java
@@ -0,0 +1,40 @@
+package ch.bbw.et.vehicle;
+
+import ch.bbw.et.person.Person;
+
+public class Auto {
+
+    private String name;
+    private Person driver;
+
+    public Auto(String name, Person driver) {
+        this.name = name;
+        this.driver = driver;
+    }
+
+    public Auto() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Person driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "name='" + name + '\'' +
+                ", driver=" + driver +
+                '}';
+    }
+}
